cb72c9429283ce50a3ff7dfb3e5746b4cc32c2ce
SfBalaba <sf.balaba0170@gmail.com>
2023-10-21 03:37:54 +0500

    findDuplicates method

--- src/main/java/ru/naumen/collection/task1/Task1.java
+++ src/main/java/ru/naumen/collection/task1/Task1.java
@@ -2,8 +2,7 @@ package ru.naumen.collection.task1;
 
 import ru.naumen.collection.task2.Ticket;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -32,12 +31,26 @@ import java.util.List;
  * @since 19.10.2023
  */
 public class Task1 {
+    public static void main(String[] args) {
+        ArrayList<User> collA = new ArrayList<User>();
+        ArrayList<User> collB = new ArrayList<User>();
+        collB.add(new User("Admin", "admin@gmail.com", new byte[]{12, 24, 5}));
+        collA.add(new User("Admin12", "admin@gmail.com", new byte[]{12, 24, 1, 5}));
+
+        collA.addAll(collB);
+        List<User> out = findDuplicates(collA, collB);
+        for (User user : out) {
+            System.out.println(user);
+        }
+    }
 
     /**
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO
-        return null;
+        Set<User> set2 = new HashSet<>(collB);
+        Set<User> result = new HashSet<>(collA);
+        result.retainAll(set2);
+        return new ArrayList<>(result);
     }
 }
